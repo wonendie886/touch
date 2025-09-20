9a00e0cb2b6b887689006230db82d78f181c793a
huangfu <598537898@qq.com>
2025-09-20 14:09:33 +0800

    change modbus baud 115200

--- HARDWARE/modbus/modbus.c
+++ HARDWARE/modbus/modbus.c
@@ -33,7 +33,7 @@ void uart4_init(void)
     __HAL_RCC_GPIOA_CLK_ENABLE();
     
     huart4.Instance = UART4;
-    huart4.Init.BaudRate = 9600;
+    huart4.Init.BaudRate = 115200;
     huart4.Init.WordLength = UART_WORDLENGTH_8B;
     huart4.Init.StopBits = UART_STOPBITS_1;
     huart4.Init.Parity = UART_PARITY_NONE;
@@ -150,11 +150,12 @@ void modbus_init(void)
 extern SemaphoreHandle_t xSharedMutex;
 static void mutex_lock(void)
 {
-    xSemaphoreTake(xSharedMutex, pdMS_TO_TICKS(50));
+    xSemaphoreTake(xSharedMutex, pdMS_TO_TICKS(100));
 }
 
 static void mutex_unlock(void)
 {
+    vTaskDelay(pdMS_TO_TICKS(50));
     xSemaphoreGive(xSharedMutex);
 }
 
@@ -171,7 +172,8 @@ static void timerStart(void)
 
 static void delayms(uint32_t nms)
 {
-    HAL_Delay(nms);
+    // HAL_Delay(nms);
+    vTaskDelay(pdMS_TO_TICKS(nms));
 }
 
 static uint32_t sendData(const void* buf, uint32_t len)
